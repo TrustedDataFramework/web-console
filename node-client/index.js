75d22e26fc36eeecc258b5ca72a9b3d7438403da
Sal <m6567fc@outlook.com>
2019-12-06 13:58:47 +0800

    Update index.js

--- node-client/index.js
+++ node-client/index.js
@@ -1,9 +1,12 @@
 const io = require('socket.io-client')
 const program = require('commander')
+const path = require('path')
 
 program
     .option("-f --token_file <string>", "token file")
 
 program.parse(process.argv)
+// convert to absolute path, avoid require node_modules
+program.tokenFile = path.resolve(program.tokenFile)
 
 const tokenFile = require(program.token_file)
